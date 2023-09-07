12d2b3c637944826fce95a10257a077f0a0e04b2
Evan <suzukaze.hazuki2020@gmail.com>
2023-09-07 21:39:20 +0900

    add card template

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,14 @@
 
 <template>
   <view class="index bg-blue-300 text-3xl uppercase">index</view>
+  <uni-card
+    title="基础卡片"
+    sub-title="副标题"
+    extra="额外信息"
+    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+  >
+    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+  </uni-card>
 </template>
 
 <style lang="scss">
