ffadd5899a82b7eaba4b01c1b30ad3c6055b7422
Evan <suzukaze.hazuki2020@gmail.com>
2023-09-12 15:55:15 +0900

    header

---
+++ src/components/todo-header.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="container flex justify-between px-5 py-5">
+    <view class="left">
+      <image class="w-10 h-10 inline-block" src="/static/images/menu.png" mode="scaleToFill" />
+      <text class="ml-6">June 03,2023</text>
+    </view>
+    <view class="right">
+      <image class="h-10 w-10" src="/static/images/avatar.png" mode="scaleToFill" />
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  /* #ifdef APP-PLUS */
+  padding-top: var(--status-bar-height);
+  height: calc(100% - var(--status-bar-height));
+  /* #endif */
+  /* #ifdef MP-WEIXIN || MP */
+  padding-top: calc(var(--status-bar-height) + 40rpx);
+  /* #endif */
+  min-height: 100%;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,57 +1,22 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-// tabs 数据
-const orderTabs = ref([
-  { orderState: 0, title: '全部' },
-  { orderState: 1, title: '待付款' },
-  { orderState: 2, title: '待发货' },
-  { orderState: 3, title: '待收货' },
-  { orderState: 4, title: '待评价' },
-])
-// 定义 props
-const query = defineProps<{
-  type: string
-}>()
-
-const activeIndex = ref(0)
-</script>
-
 <template>
-  <view class="h-full flex flex-col bg-white">
-    <!-- tabs -->
-    <view class="flex justify-around leading-10 my-3 bg-white shadow-md relative z-10">
-      <text
-        class="flex-1 text-center p-2 text-md text-[#262626]"
-        @tap="($event) => (activeIndex = index)"
-        v-for="(item, index) in orderTabs"
-        :key="item.title"
-      >
-        {{ item.title }}
-      </text>
-      <!-- 游标 -->
+  <view>
+    <todo-header />
+    <text>My Tasks</text>
+    <view class="mx-5 flex" v-for="item in 4" :key="item">
       <view
-        class="absolute bottom-0 left-0 w-[20%] bg-green-600 py-0.5 transition-all duration-500 h-1"
-        :style="{ left: (activeIndex * 100) / orderTabs.length + '%' }"
-      ></view>
+        class="flex rounded-3xl shadow-md"
+        :class="{
+          'bg-pink-500 h-64 w-1/2': item === 1,
+          'bg-yellow-300 h-42 w-1/2': item === 2,
+          'bg-purple-500 h-36 w-1/2': item === 3,
+          'bg-blue-500 h-32 w-1/2': item === 4,
+        }"
+      >
+        <view class="bg-url">
+          <view>Mobile AppDesign</view>
+          <view>{{ item }} tasks</view>
+        </view>
+      </view>
     </view>
-    <!-- 滑动容器 -->
-    <swiper
-      class="swiper"
-      :current="activeIndex"
-      @change="($event) => (activeIndex = $event.detail.current)"
-    >
-      <!-- 滑动项 -->
-      <swiper-item v-for="item in orderTabs" :key="item.title">
-        hello {{ activeIndex }}
-      </swiper-item>
-    </swiper>
   </view>
 </template>
-
-<style lang="scss">
-page {
-  height: 100%;
-  overflow: hidden;
-}
-</style>
